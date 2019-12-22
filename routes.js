3b47d125a8c9eb4ba6e6ab610056d2aa3d3df1ca
Riaz Laskar <riazcool77@gmail.com>
2019-12-22 18:53:49 +0530

    ios dev

--- routes.js
+++ routes.js
@@ -1,48 +1,40 @@
 /* eslint-disable react-native/no-inline-styles */
 import React from 'react';
 
-import { View, Text } from 'react-native';
+import {View, Text} from 'react-native';
 
-import {
-    createAppContainer,
-    createSwitchNavigator
-} from 'react-navigation';
+import {createAppContainer, createSwitchNavigator} from 'react-navigation';
 
-
-import {
-    createStackNavigator,
-} from 'react-navigation-stack';
-
+import {createStackNavigator} from 'react-navigation-stack';
 
 import Login from './src/screens/Login';
-import TimesheetLogger from './src/screens/TimesheetLogger'
-import SwitchScreen from './src/screens/SwitchScreen'
-
+import TimesheetLogger from './src/screens/TimesheetLogger';
+import SwitchScreen from './src/screens/SwitchScreen';
 
 const Main = createStackNavigator(
-    {
-        Home: { screen: Login },
-        Logger: { screen: TimesheetLogger }
+  {
+    Home: {screen: Login},
+    Logger: {screen: TimesheetLogger},
+    Switch: {screen: SwitchScreen},
+  },
+  {
+    headerMode: 'none',
+    navigationOptions: {
+      headerVisible: false,
     },
-    {
-        headerMode: 'none',
-        navigationOptions: {
-            headerVisible: false,
-        },
-    }
+  },
 );
 
 const Routes = createSwitchNavigator(
-    {
-        Switch: { screen: SwitchScreen },
-        Login: Main,
+  {
+    Login: Main,
+  },
+  {
+    headerMode: 'none',
+    navigationOptions: {
+      headerVisible: false,
     },
-    {
-        headerMode: 'none',
-        navigationOptions: {
-            headerVisible: false,
-        },
-    }
+  },
 );
 
 export default RouteContainer = createAppContainer(Routes);
